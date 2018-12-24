6531480af22850ab118261fe4827c5e2d0a49cad
Sreeni <segireddy_s@yahoo.com>
2018-12-24 13:48:20 -0500

    comming my new crm project

---
+++ MyFreeCRM/src/main/java/com/jcom/qa/pages/DealsPage.java
@@ -0,0 +1,7 @@
+package com.jcom.qa.pages;
+
+import com.jcom.qa.base.TestBase;
+
+public class DealsPage extends TestBase{
+
+}
---
+++ MyFreeCRM/src/test/java/com/jcom/qa/testcases/LoginPageTest.java
@@ -0,0 +1,50 @@
+package com.jcom.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.jcom.qa.base.TestBase;
+import com.jcom.qa.pages.HomePage;
+import com.jcom.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+	
+
+	
+	@Test(priority=2)
+	public void crmLogoImageTest(){
+		boolean flag = loginPage.validateCRMImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest(){
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+
+}
